12461eaea5a53ce7048179f419bba6b26b9c4ecf
Kish Siva <rodes88@msn.com>
2019-04-07 16:09:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,26 +6,16 @@ def using_unshift (array, string)
   array.unshift(string)
 end
 
-def using_pop (array, string)
-  array.pop(string)
+def using_pop (array)
+  array.pop
 end
 
 def pop_with_args(array, string)
   array.pop(string)
 end
 
-describe "#pop_with_args" do
-  before(:each) do 
-    @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-    @chars_arya_killed = pop_with_args(@chars_in_game_of_thrones)
-  end 
-  it "takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them" do 
-    expect(@chars_arya_killed).to eq(["Stable Boy", "Sandor Clegane"])
-  end 
-
-  it "decreases the length of the array by 2" do
-    expect(@chars_in_game_of_thrones.size).to eq(2) 
-  end
+def using_shift (array, string)
+  array.shift(string)
 end
 
 describe "#using_shift" do 
